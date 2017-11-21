29a002274d3c87a77caa02e150f7bbfc26ee9e2a
Dmitry Lukanin <d.lukanin@qiwi.com>
2017-11-21 12:51:03 +0300

    auth authenticated param rename

--- app/auth/default.ts
+++ app/auth/default.ts
@@ -1,12 +1,12 @@
 import {IAuth} from './interfaces';
 
 export default class Auth implements IAuth {
-    private authenticated: Set<string> = new Set<string>();
+    protected _authenticated: Set<string> = new Set<string>();
 
     public isAuthenticated(id: string): boolean {
-        return this.authenticated.has(id);
+        return this._authenticated.has(id);
     }
     public authenticate(id: string): void {
-        this.authenticated.add(id);
+        this._authenticated.add(id);
     }
 }
